cd7f8a83c5fe7b4cec3411d0a2ef2596576a44a6
simpleneeraj <simplneeraj@gmail.com>
2023-11-02 19:44:33 +0530

    done with simple

--- components/accordion/index.tsx
+++ components/accordion/index.tsx
@@ -8,8 +8,14 @@ import { AccordionType } from '@/typings/accordion';
 import Arrowdown from '../icons/Arrowdown';
 
 const Accordion = () => {
+  /**
+   * @description This is the state of the accordion
+   */
   const [state, setState] = React.useState<AccordionType>(accordionData[0]);
 
+  /**
+   * @description This function is used to set the state of the accordion
+   */
   const onSelect = React.useCallback((item: AccordionType) => {
     setState(item);
   }, []);
@@ -24,7 +30,50 @@ const Accordion = () => {
       <div className={css['grid']}>
         <div className={css['list']}>
           <ul>
-            <li>
+            {accordionData.map((item, index) => {
+              const active = state.question === item.question;
+              return (
+                <li key={index}>
+                  <button
+                    onClick={() => {
+                      if (!active) {
+                        onSelect(item);
+                      }
+                    }}
+                    className={css['list-item']}
+                    style={{
+                      //   borderBottom: active ? 'none' : '1px solid #e1e1e1',
+                      borderTop: active ? 'none' : '1px solid #e1e1e1',
+                      padding: active ? '0px' : '1rem 0px',
+                      transition: 'padding 0.3s ease-in-out',
+                    }}
+                  >
+                    <span>{item.question}</span>
+                    <span>
+                      <Arrowdown
+                        height={24}
+                        width={24}
+                        fill={
+                          state.question === item.question ? '#fff' : '#000'
+                        }
+                      />
+                    </span>
+                  </button>
+                  <p
+                    className={css['content']}
+                    style={{
+                      maxHeight: active ? '300px' : '0px',
+                      overflow: 'hidden',
+                      transition: 'max-height 0.3s ease-in-out',
+                      padding: active ? '1rem 0px' : '0px',
+                    }}
+                  >
+                    {item.answer}
+                  </p>
+                </li>
+              );
+            })}
+            {/* <li>
               <button
                 onClick={() => onSelect(accordionData[0])}
                 className={css['list-item']}
@@ -34,7 +83,7 @@ const Accordion = () => {
               <span>
                 <Arrowdown />
               </span>
-            </li>
+            </li> */}
           </ul>
         </div>
 
